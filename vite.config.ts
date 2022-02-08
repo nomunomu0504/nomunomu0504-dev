83c3895ae2842981ccdd4f13334ed329af1f0edf
h.nomura <>
2022-02-09 02:29:05 +0900

    setting pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   root: "./src",
-  plugins: [react()],
+  plugins: [react(), , VitePWA()],
   server: {
     open: true,
   },
   build: {
     outDir: "../dist",
   },
-})
+});
