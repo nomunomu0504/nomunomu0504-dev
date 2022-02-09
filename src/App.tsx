45bf04edf2f568e958c953698fdb3fcf788e3830
h.nomura <>
2022-02-09 23:29:33 +0900

    add chakra-ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { RoconRoot } from "rocon/react";
+import { ChakraProvider } from "@chakra-ui/react";
 import { Routes } from "~/components/routes";
 
 const App: React.VFC = () => {
   return (
     <div>
       <RoconRoot>
-        <Routes />
+        <ChakraProvider>
+          <Routes />
+        </ChakraProvider>
       </RoconRoot>
     </div>
   );
