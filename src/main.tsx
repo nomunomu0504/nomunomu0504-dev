7c79f88ffa49c4080f46a933726fafd909deb743
h.nomura <>
2022-02-09 23:24:57 +0900

    modify eslint / prettier / env / editorconfig

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 import App from "./App";
 
 import { registerSW } from "virtual:pwa-register";
-registerSW();
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,3 +10,6 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
+
+// サービスワーカーを登録
+registerSW();
--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,29 @@
-import react from "@vitejs/plugin-react";
 import reactRefresh from "@vitejs/plugin-react-refresh";
+import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
 import { defineConfig } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
+import viteReactJsx from "vite-react-jsx";
 import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), reactRefresh(), VitePWA()],
+  plugins: [
+    reactRefresh(),
+    viteReactJsx(),
+    VitePWA({
+      strategies: "injectManifest",
+      manifest: {
+        // content of manifest
+      },
+    }),
+    vanillaExtractPlugin(),
+  ],
   server: {
     open: true,
   },
   build: {
     outDir: "../dist",
+    emptyOutDir: true,
   },
   resolve: {
     alias: [
